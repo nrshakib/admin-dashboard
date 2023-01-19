150c78e006590b08778ff6d3b1dbbb991575dc89
NRS\nrsha <nrshakib007@gmail.com>
2023-01-19 14:49:53 +0600

    users page

--- src/Pages/New/New.js
+++ src/Pages/New/New.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import Navbar from '../../Components/Navbar/Navbar';
+import Sidebar from '../../Components/Sidebar/Sidebar';
+import './New.scss';
 
 const New = () => {
     return (
-        <div>
-            <h2>New</h2>
+        <div className="new">
+            <Sidebar></Sidebar>
+            <div className="newContainer">
+                <Navbar></Navbar>
+                <div className="top">
+                    <h1>Add New User</h1>
+                </div>
+                <div className="bottom">
+                    <div className="left">
+                        <img src="https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+                            alt="" />
+                    </div>
+                    <div className="right">R</div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Single/Single.js
+++ src/Pages/Single/Single.js
@@ -1,9 +1,55 @@
 import React from 'react';
+import './Single.scss';
+import Sidebar from '../../Components/Sidebar/Sidebar';
+import Navbar from '../../Components/Navbar/Navbar';
+import Chart from '../../Components/Chart/Chart';
+import ListTable from '../../Components/ListTable/ListTable';
 
 const Single = () => {
     return (
-        <div>
-            <h2>Single</h2>
+        <div className="single">
+            <Sidebar></Sidebar>
+            <div className="singleContainer">
+                <Navbar></Navbar>
+                <div className="top">
+                    <div className="left">
+                        <div className="editButton">Edit</div>
+                        <h1 className="title">Information</h1>
+                        <div className="items">
+                            <img src="https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+                                alt=""
+                                className="itemImg" />
+                            <div className="details">
+                                <h1 className="itemTitle">Joe Hare</h1>
+                                <div className="detailItem">
+                                    <span className="itemKey">Email:</span>
+                                    <span className="itemKey">joehare@gmail.com</span>
+                                </div>
+                                <div className="detailItem">
+                                    <span className="itemKey">Phone:</span>
+                                    <span className="itemKey">+12277546</span>
+                                </div>
+                                <div className="detailItem">
+                                    <span className="itemKey">Address:</span>
+                                    <span className="itemKey">222 A,Kaber Street</span>
+                                </div>
+                                <div className="detailItem">
+                                    <span className="itemKey">Country:</span>
+                                    <span className="itemKey">UK</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="right">
+                        <Chart aspect={3 / 1} title="User Spending (Last 6 Months)"></Chart>
+                    </div>
+                </div>
+                <div className="bottom">
+                    <h1 className="title">Last Transactions</h1>
+                    <ListTable></ListTable>
+                </div>
+            </div>
+
         </div>
     );
 };
